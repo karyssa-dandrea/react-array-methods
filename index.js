3e1f0464143860182cab405114252cc5bdfba3f8
karyssa-dandrea <karyssadandrea@gmail.com>
2021-12-07 10:43:45 -0800

    passed two tests

--- index.js
+++ index.js
@@ -1,14 +1,20 @@
+import { brands, countries } from './data';
+
 // INPUT: the array of countries from data.js
 // OUTPUT: an array of the names
 // REQS: use .map
 
-export const getCountryNames = (countries) => {};
+export const getCountryNames = (countries) => {
+  return countries.map((country) => country.name);
+};
 
 // INPUT: the array of countries from data.js
 // OUTPUT: an array of the names and their iso2 country codes using .map
 // REQS: use .map
 
-export const getCountryCodes = (countries) => {};
+export const getCountryCodes = (countries) => {
+  return countries.map((country) => [country.name, country.iso2]);
+};
 
 // INPUT: the array of countries from data.js
 // OUTPUT: only countries in Oceana
